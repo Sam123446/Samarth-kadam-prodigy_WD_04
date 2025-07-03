595698321f685df92c3122932c861cc20622e170
Sam123446 <kadamsamarth91@gmail.com>
2025-07-03 16:24:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder for future JavaScript functionality
+// You can add interactive features here like:
+// - Dynamic project filtering
+// - Scroll animations
+// - Theme toggles
+// - Form validations
+console.log("Welcome to Samarth Kadam's Portfolio!");
+
